dea73cc4aaad0b9a4d238d0a90acfb61ca737926
root <root@hdp-server-01.localdomain>
2013-12-20 21:26:01 +0800

    peer probe & detach

---
+++ glusterwrap/peer/detach.py
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*
+# Copyright © 2013 Computer Network Information Center, Chinese Academy of Sciences
+#
+# Author: Jing Shao <jingshao@cnic.cn>
+
+import re
+import glusterwrap as gw
+
+def detach(hostname, targetname):
+    return _detach(hostname=hostname, targetname=targetname)
+
+def _detach(hostname, targetname):
+    # Get gluster cmd path
+    gluster_path = None
+    tmp_list = gw.util.which('gluster')
+    if tmp_list != [] and tmp_list != None:
+        gluster_path = tmp_list[0]
+    else:
+        # There is no gluster on this server
+        return None
+    
+    program = [gluster_path,
+            "--remote-host=%s" % hostname,
+            "peer",
+            "detach",
+            targetname]
+
+    response = gw.util.do_process_resp(program)
+
+    if response == None:
+        return [False, "Run subprocess error."]
+    else:
+        if response[0].count('success') > 0:
+            return [True, response]
+        else:
+            return [False, response]
--- test.py
+++ test.py
@@ -9,20 +9,14 @@ def print_list(objList):
     print jsonDumpsIndentStr
 
 if __name__ == "__main__":
-#    t1 = datetime.datetime.now()
-#    peer_status_v2 = gluster.peer.status_v2(remotehost='10.0.0.54')
-#    t2 = datetime.datetime.now()
-    peer_status = gw.peer.status(remotehost=gw.util.get_ip())
-#    t3 = datetime.datetime.now()
-#    print 'peer status_v2:'
-#    print_list(peer_status_v2)
-    print 'peer status:'
-    print_list(peer_status)
-#    print 'status_v2 cost: ' + str((t2 - t1).microseconds)
-#    print 'status: ' + str((t3 - t2).microseconds)
+#    peer_status = gw.peer.status(remotehost=gw.util.get_ip())
+#    print_list(peer_status)
+#    print '-------------'
+#    volume_status = gw.volume.info(remotehost=gw.util.get_ip())
+#    print 'volume status:'
+#    print_list(volume_status)
 
-    print '-------------'
-    volume_status = gw.volume.info(remotehost=gw.util.get_ip())
-    print 'volume status:'
-    print_list(volume_status)
+     [flag, msg] = gw.peer.detach(hostname="localhost", targetname="10.0.0.240")
+     print flag
+     print msg
     
